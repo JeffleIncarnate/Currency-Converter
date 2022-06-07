33706d99247cb6a99f655ffce6556c65b1779bbf
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-06-08 11:55:39 +1200

    MAJOR BRAIN DAMAGE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Currency_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Currencies = new List<string>();
+            Currencies.AddRange(new string[] {"", "",
+            "", "", ""
+            });
+
+
+
+            // var url = "https://api.apilayer.com/currency_data/convert?to=GBP&from=USD&amount=100";
+
+            // var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            // httpRequest.Headers["apikey"] = "uQvrOvY98Px1fsgLmvDtkCSEOtYtWpLK";
+            // httpRequest.Headers["Authorization"] = "apikey: uQvrOvY98Px1fsgLmvDtkCSEOtYtWpLK";
+
+
+            // var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            // using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            // {
+            //     var result = streamReader.ReadToEnd();
+            //     JObject resultJson = JObject.Parse(result);
+            // }
+        }
+    }
+}
